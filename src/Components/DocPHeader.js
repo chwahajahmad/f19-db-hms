a84bf771a0141c2551cf87b0ce9b7558f78104a7
chwahajahmad <chwahajahmad@gmail.com>
2021-07-01 23:37:12 +0500

    Modal Added

--- src/Components/DocPHeader.js
+++ src/Components/DocPHeader.js
@@ -1,16 +1,23 @@
-import React from "react";
-import { Row, Col, Button } from "antd";
+import React, { useState } from "react";
+import { Row, Col, Button, Modal } from "antd";
 function DocPHeader({ message }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
   return (
     <Row gutter={16} justify="center">
       <Col span={16} xs={24} sm={16}>
         <h1>{message}</h1>
       </Col>
       <Col span={4} xs={24} sm={4}>
-        <Button type="primary" danger>
+        <Button type="primary" danger onClick={showModal}>
           Edit Data
         </Button>
       </Col>
+      <Modal title="Edit Data" visible={isModalVisible} footer={null}></Modal>
     </Row>
   );
 }
